98ca7eda22c3f4b772b60060ddc3074097aac7f7
MM02-sudo <dandjimalcolm2@gmail.com>
2026-02-13 09:44:51 +0100

    testing stuff for learning

---
+++ go.mod
@@ -0,0 +1,7 @@
+module kilo
+
+go 1.25.6
+
+require golang.org/x/term v0.40.0
+
+require golang.org/x/sys v0.41.0 // indirect
--- kilo.go
+++ kilo.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"golang.org/x/term"
 )
 
+func enableRawMode() *term.State {
+	oldTerminalState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	return oldTerminalState
+}
+
 func main() {
+	oldTerminalState := enableRawMode()
+	defer term.Restore(int(os.Stdin.Fd()), oldTerminalState)
+
+	// we create a slice that stores 1 byte
 	buf := make([]byte, 1)
 	for {
+		// we read the byte sotred in the slice
 		n, err := os.Stdin.Read(buf)
 		if err != nil || n == 0 {
 			break
 		}
-		if buf[0] == 'q' {
+		// if the user types CTRL+Q it quits.
+		// 17 because in ASCII Q + 81 and CTRL removes 64 so 81-64 = 17
+
+		if buf[0] == 17 {
 			break
 		}
+		// writing back to screen each key stroke of the user
+		fmt.Printf("%d\r\n", buf[0])
 
 	}
 }
