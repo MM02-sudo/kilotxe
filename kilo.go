478b2072989bfd164dace2adb2c08320f37594c8
MM02-sudo <dandjimalcolm2@gmail.com>
2026-02-17 18:11:16 +0100

    fixed control characters and printing out arrow keys for tesiting

--- kilo.go
+++ kilo.go
@@ -8,6 +8,7 @@ import (
 )
 
 func enableRawMode() *term.State {
+	// old terminal state
 	oldTerminalState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
 		panic(err)
@@ -20,13 +21,30 @@ func main() {
 	defer term.Restore(int(os.Stdin.Fd()), oldTerminalState)
 
 	// we create a slice that stores 1 byte
-	buf := make([]byte, 1)
+	buf := make([]byte, 3)
 	for {
+		buf = make([]byte, 3)
 		// we read the byte sotred in the slice
 		n, err := os.Stdin.Read(buf)
 		if err != nil || n == 0 {
 			break
 		}
+
+		// cheking for arrow keys
+		if n == 3 && buf[0] == 27 && buf[1] == '[' {
+			switch buf[2] {
+			case 'A':
+				fmt.Printf("Up arrow\r\n")
+			case 'B':
+				fmt.Printf("Down arrow\r\n")
+			case 'C':
+				fmt.Printf("Right arrow\r\n")
+			case 'D':
+				fmt.Printf("Left arrow\r\n")
+			}
+			continue
+		}
+
 		// if the user types CTRL+Q it quits.
 		// 17 because in ASCII Q + 81 and CTRL removes 64 so 81-64 = 17
 
@@ -35,8 +53,13 @@ func main() {
 		if buf[0] == ctrlq {
 			break
 		}
-		// writing back to screen each key stroke of the user
-		fmt.Printf("%d\r\n", buf[0])
-
+		// print other har (,only id n == 1)
+		if n == 1 {
+			if buf[0] < 32 || buf[0] == 127 {
+				fmt.Printf("%d\r\n", buf[0])
+			} else {
+				fmt.Printf("%d ('%c')\r\n", buf[0], buf[0])
+			}
+		}
 	}
 }
